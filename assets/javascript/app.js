a99d724341ec6356649e0c3992d70b5acb26fa02
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-07-31 04:56:10 -0400

    added axios

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+var axios = require("axios");
+
+function getBands(band) {
+    axios
+        .get("")
+        .then(function (response) {
+
+
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            if (error.response) {
+
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+
+                console.log(error.request);
+            } else {
+
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+
+};
+
+getBands();
